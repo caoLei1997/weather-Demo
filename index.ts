d26c3dbe9db6b0388fa3f3c36baf156c78bd8705
caolei <caoLei1997@126.com>
2020-11-25 11:01:09 +0800

    add comander colors

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-// console.log('hello ts-weather123')
+const { program } = require('commander');
+import colors from 'colors'
+program
+  .version('0.1.0')
+  .option('-c, --city [name]', 'Add city name')
+program.parse(process.argv);
+
+if (process.argv.slice(2).length === 0) {
+  program.outputHelp(colors.red);
+  process.exit();
+}
